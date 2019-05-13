0bb0c8ec438b65a26e369b1181c4305fb029d08c
liangyuhit <312592076@qq.com>
2019-05-13 14:42:10 +0200

    2019-5-13 14:42:05 from PTB

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+'''
+Created on 13.05.2019
+
+@author: yu03
+'''
 
 a = '2019-5-6 20:03:16'
 b = '2019-5-6 20:05:02 from Github'
@@ -10,5 +15,6 @@ g = '2019-05-07 22:26:23 from Home'
 h = '2019-05-07 22:27:10 from Github'
 i = '2019-5-8 14:15:11 from PTB'
 j = '2019-5-8 14:18:49 from Github'
+k = '2019-5-13 14:41:42 from PTB'
 
-print(h)
+print(k)
