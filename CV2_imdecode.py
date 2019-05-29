3888831252c406e74df2695b71ccfcb7d8cf8215
liangyuhit <312592076@qq.com>
2019-05-29 14:28:05 +0200

    2019-5-29 14:27:59 from PTB

---
+++ CV2_imdecode.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 27.05.2019
+
+@author: yu03
+'''
+import numpy as np  
+import urllib  
+import cv2  
+  
+# url = 'http://www.pyimagesearch.com/wp-content/uploads/2015/01/google_logo.png'  
+# resp = urllib.urlopen(url)  
+# image = np.asarray(bytearray(resp.read()), dtype="uint8")  
+# image = cv2.imdecode(image, cv2.IMREAD_COLOR)  
+# cv2.imshow('URL2Image',image)  
+# cv2.waitKey()
