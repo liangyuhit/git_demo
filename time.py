16ed5bac8fd10b40f189df60026386fd54fde48c
liangyuhit <312592076@qq.com>
2019-06-25 18:37:18 +0200

    2019-6-25 18:37:15

---
+++ time.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 25.06.2019
+
+@author: yu03
+'''
+
+
+import datetime
+import time
+import pytz
+
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print('Local time:     ', now)
+
+now_utc = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+print('UTC time:      ', now_utc)
+
+timezone = pytz.timezone('Asia/Shanghai')
+now_timezone = datetime.datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
+print('China time:     ', now_timezone)
+
+timestamp = int(time.time())
+# timestamp = 1561471260
+print('Timestamp:     ', timestamp)
+
+time = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+print('Convert Local:     ', time)
+
+time_utc = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+print('Convert UTC:     ', time_utc)
+
+timezone = pytz.timezone('Asia/Shanghai')
+time_timezone = datetime.datetime.fromtimestamp(timestamp, tz=timezone).strftime('%Y-%m-%d %H:%M:%S')
+print('China time:     ', now_timezone)
