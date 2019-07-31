838663b8182edd749f1975511aad86b2bf801a53
liangyuhit <312592076@qq.com>
2019-07-31 11:14:24 +0200

    2019-7-31 11:14:13

--- save_load_dat.py
+++ save_load_dat.py
@@ -6,9 +6,19 @@ Created on 31.07.2019
 '''
 import numpy as np
 
-file_name = 'test.npy'
+file_name = r'C:\Users\yu03\eclipse-workspace\git_demo\test.npy'
 
-a = np.zeros(10)
-print(a)
-f = open(file_name,'wb')
-np.save(f, a)
+if 0:
+    for i in range(10):
+        a = np.zeros(3)
+        b = np.random.randint(0,5,3)
+        c = 100
+        out = np.array([a,b,c])
+        print(out)
+        f = open(file_name,'ab')
+        np.save(f, out)
+
+if 1:
+    f = open(file_name, 'rb')
+    a = np.load(f, allow_pickle=True)
+    print(a)
