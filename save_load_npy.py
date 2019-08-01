de6ea424aeb6d197f0407d0b0f694999fabc1912
liangyuhit <312592076@qq.com>
2019-08-01 15:23:51 +0200

    2019-8-1 15:23:49

--- save_load_npy.py
+++ save_load_npy.py
@@ -6,8 +6,15 @@ Created on 31.07.2019
 '''
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+def fit_func_line(x, p, q):
+    return p*x+q
+
 
 file_name = r'C:\Users\yu03\eclipse-workspace\git_demo\test.npy'
+
 
 if 0:
     for i in range(100):
@@ -25,9 +32,32 @@ if 1:
     f_size = os.fstat(f.fileno()).st_size
     print(f_size)
     lines = []
+    time_sequence = []
     while f.tell() < f_size:
-        print(f.tell())
+#         print(f.tell())
         line = np.load(f, allow_pickle=True)
-        print(line)
-        lines.append(line)
+        print(line[0], line[1])
+        lines.append(line[0])
+        time_sequence.append(line[1])
     print(len(lines))
+
+    
+    x_axis = np.linspace(0, len(time_sequence)-1, len(time_sequence))
+#     time_sequence = time_sequence[500:3500]
+#     x_axis = x_axis[500:3500]
+    fit_params = curve_fit(fit_func_line, np.linspace(0, len(time_sequence)-1, len(time_sequence)), time_sequence)
+    [p, q] = fit_params[0]
+    print(p)
+    linear_fit = np.linspace(0, len(time_sequence)-1, len(time_sequence)) * p + q
+    [p, q] = np.polyfit(x_axis, time_sequence, 1)
+    linear_fit = x_axis * p + q
+    
+    plt.figure(1)
+    plt.subplot(2,1,1)
+    plt.plot(time_sequence, 'b', marker=' ')
+    plt.plot(linear_fit, 'r', marker=' ')
+    plt.grid(which='both', axis='both')
+    plt.subplot(2,1,2)
+    plt.plot(time_sequence-linear_fit)
+    plt.grid(which='both', axis='both')
+    plt.show()
