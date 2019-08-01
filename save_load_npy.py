ec6a93ebcc0f52053e033d6452f923ef0a87a0dd
liangyuhit <312592076@qq.com>
2019-08-01 10:53:09 +0200

    2019-8-1 10:53:07

--- save_load_npy.py
+++ save_load_npy.py
@@ -5,11 +5,12 @@ Created on 31.07.2019
 @author: yu03
 '''
 import numpy as np
+import os
 
 file_name = r'C:\Users\yu03\eclipse-workspace\git_demo\test.npy'
 
 if 0:
-    for i in range(10):
+    for i in range(100):
         a = np.zeros(3)
         b = np.random.randint(0,5,3)
         c = 100
@@ -17,9 +18,16 @@ if 0:
         print(out)
         f = open(file_name,'ab')
         np.save(f, out)
+        f.close()
 
 if 1:
     f = open(file_name, 'rb')
-    a = np.load(f, allow_pickle=True)
-    print(a)
-    print(len(a))
+    f_size = os.fstat(f.fileno()).st_size
+    print(f_size)
+    lines = []
+    while f.tell() < f_size:
+        print(f.tell())
+        line = np.load(f, allow_pickle=True)
+        print(line)
+        lines.append(line)
+    print(len(lines))
--- save_load_txt.py
+++ save_load_txt.py
@@ -15,7 +15,7 @@ header = ['%s\n' %file_name,
         '-------------------------------------------------\n',
         ]
 
-if 1:
+if 0:
     #### Header Writing
     f = open(file_name,'w')
     f.writelines(header)
@@ -29,7 +29,7 @@ if 1:
         f.close()
         print(a)
 
-if 0:
+if 1:
     f = open(file_name,'r')
     line_header=''
     while line_header[0:4] != '----':
@@ -37,7 +37,7 @@ if 0:
         print(line_header.strip())
     
     lines = f.readlines()
-#     print(lines)
+    print(lines)
     for line in lines:
         x = line.strip().split(' ')
         x = [int(k) for k in x]
