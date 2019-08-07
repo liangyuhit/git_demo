37044c19de941a9da0a9500fd98ae2c1d730fc61
liangyuhit <312592076@qq.com>
2019-08-07 18:35:38 +0200

    2019-8-7 18:35:37

--- PSD_test.py
+++ PSD_test.py
@@ -8,8 +8,10 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
-f = 1e2
-N = 1e4
+f1, f2, f3 = 50, 100, 200
+A1, A2, A3 = 0.1, 1, 10
+phi1, phi2, phi3 = 0, 1, 2
+N = 10000
 fs = 1e4
 x = np.arange(N)/fs
 t = N/fs
@@ -17,17 +19,18 @@ print('Points number: ', N)
 print('fs: ', fs, 'Hz')
 print('time: ', t, 's')
 
-sig = np.cos(2*np.pi*f*x)
+sig = A1*np.cos(2*np.pi*f1*x+phi1) + A2*np.cos(2*np.pi*f2*x+phi2) + A3*np.cos(2*np.pi*f3*x+phi3)
 noise = np.random.rand(len(x))/10
 sig += noise
 
-f_line, PS = signal.welch(sig, fs, nperseg=1024)
+f_line, PS = signal.welch(sig, fs, nperseg=len(sig))
 PSD = np.sqrt(PS)
-plt.figure('Signal')
-plt.plot(x,sig)
-plt.xlabel('sample')
-plt.ylabel('Amplitude [V]')
-plt.grid(which='major', axis='both')
+
+# plt.figure('Signal')
+# plt.plot(sig)
+# plt.xlabel('sample')
+# plt.ylabel('Amplitude [V]')
+# plt.grid(which='major', axis='both')
 
 plt.figure('PSD')
 plt.loglog(f_line, PSD)
