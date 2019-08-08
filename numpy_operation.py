62afdceced69e1c0c2b9af764c4311a5cf47035b
liangyuhit <312592076@qq.com>
2019-08-08 17:09:42 +0200

    2019-8-8 17:09:40

---
+++ numpy_operation.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 08.08.2019
+
+@author: yu03
+'''
+
+import numpy as np
+
+a = np.array([np.array([[1,2,3],[3,4,5]]), 1])
+print(a)
+a = a[0]
+print(a)
+print(a[0])
