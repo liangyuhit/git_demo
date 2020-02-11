e23fd2e457ae9b6e9695ad83a588c73712d19ec2
liangyuhit <312592076@qq.com>
2020-02-11 10:11:01 +0100

    2020-2-11 10:10:59

--- test.py
+++ test.py
@@ -31,4 +31,16 @@ k = '2019-7-25 13:45:05 from PTB'
 
 a = hermval(2, [0,0,1])
 
+from pylab import *
+
+axes([0.1,0.1,.8,.8])
+xticks([]), yticks([])
+text(0.6,0.6, 'axes([0.1,0.1,.8,.8])',ha='center',va='center',size=20,alpha=.5)
+
+axes([0.2,0.2,.3,.3])
+xticks([]), yticks([])
+text(0.5,0.5, 'axes([0.2,0.2,.3,.3])',ha='center',va='center',size=16,alpha=.5)
+
+show()
+
 print(a)
